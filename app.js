ae7d80d8a68c5b2529ed7c07787a8b12d9e117d5
Grillai <Andrea.info19@gmail.com>
2022-08-17 18:32:06 +0200

    Sito Sport

--- app.js
+++ app.js
@@ -16,4 +16,15 @@ const navSlide = () =>{
    
 }
 
+
+const toTop= document.querySelector(".to-top");
+    window.addEventListener("scroll",() =>{
+        if(window.pageYOffset >300){
+            toTop.classList.add("active")
+        }
+        else{
+            toTop.classList.remove("active")
+        }
+})
+
 navSlide();
