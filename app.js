7d451181e443a02cad213e45bb2ae432212ca766
Grillai <Andrea.info19@gmail.com>
2022-09-29 23:17:44 +0200

    SITO

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
-const navSlide = () =>{
-    const hamburger  = document.querySelector('.hamburger');
-    const nav = document.querySelector('.nav-links');
-   
+const navSlide = () => {
+  const hamburger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".nav-links");
 
-    hamburger.addEventListener('click' , () =>{
-        nav.classList.toggle('active');
-        hamburger.classList.toggle('active');
-    });
+  hamburger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    hamburger.classList.toggle("active");
+  });
 
-    document.querySelectorAll(".nav-links").forEach(n =>n.addEventListener("click", ()=>{
-        nav.classList.remove('active');
-        hamburger.classList.remove('active');
-    }));
-        
-   
-}
+  document.querySelectorAll(".nav-links").forEach((n) =>
+    n.addEventListener("click", () => {
+      nav.classList.remove("active");
+      hamburger.classList.remove("active");
+    })
+  );
+};
 
-
-const toTop= document.querySelector(".to-top");
-    window.addEventListener("scroll",() =>{
-        if(window.pageYOffset >300){
-            toTop.classList.add("active")
-        }
-        else{
-            toTop.classList.remove("active")
-        }
-})
+const toTop = document.querySelector(".to-top");
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 300) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
 
 navSlide();
